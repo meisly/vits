8f120f8faf767fd59a91f2b17a84c86735c6964a
Tierra <sr.undershaft@gmail.com>
2020-01-15 15:07:49 -0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vits.Startup))]
+namespace Vits
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
